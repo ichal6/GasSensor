f15839e1ce1aef5be5fb862e9b7dab6c81de641d
Michael Lechowicz <ichal.lechowicz@gmail.com>
2021-11-18 15:34:49 +0100

    Implement support for button on

--- OHMMeter.ino
+++ OHMMeter.ino
@@ -14,17 +14,34 @@ float value_percent = 0;
 float max_value = 89.91;
 float min_value = 1.93;
 float range_value = max_value - min_value;
+int sensorVal = 0;
 
 void setup() 
 {
  lcd.init();
  lcd.backlight(); //turn on backlight
  Serial.begin(9600); //start communication via UART with speed 9600 baud/sec (symbols/seconds)
+ //konfiguracja pinu3 jako wejście i aktywowanie wbudowanego rezystora podciągającego
+ pinMode(3, INPUT_PULLUP);
 }
 
 void loop()
 {
+  sensorVal = digitalRead(3);   //odczytanie stanu przycisku i przypisanie go do zmiennej sensorVal
   a2d_data=analogRead(A0); // READ data from analog A0 port (0 - 1023)
+  Serial.println(sensorVal);     //przesłanie odczytanej wartości zmiennej sensorVal do portu szeregowego
+ // Należy pamiętać, że po aktywacji pull-up logika przycisku jest odwrócona
+  // Jeśli przycisk jest nie naciśnięty to jego stan jest wysoki (HIGH),
+  // a stan niski (LOW) kiedy jest naciśnięty. Dioda będzie zapalona kiedy przycisk
+  // zostanie naciśnięty:
+  if (sensorVal == HIGH) {
+    lcd.noBacklight();
+    delay(1000);
+  } else {
+    lcd.backlight();
+    delay(4000);
+  }
+  
   if(a2d_data)
   {
     buffer=a2d_data*Vin;
@@ -34,7 +51,7 @@ void loop()
     ///Serial.print("buffer = "); 
     //Serial.println(buffer);
     R_sensor=R_reference*buffer; // calculate unknown R2 from Ohm rule
-    value_percent = ((R_sensor-min_value)/range_value)*100;
+    value_percent = ((R_sensor-min_value)/range_value)*100; //calculate actual fuel in percent
     lcd.clear();
     lcd.setCursor(4,0);
     lcd.print("ohm meter");
@@ -43,7 +60,7 @@ void loop()
     lcd.print(round(value_percent));
     lcd.print("\% FUEL");
     Serial.println(R_sensor);
-    delay(3000);
+    //delay(3000);
     //lcd.noBacklight();
   }
 }
