c7537793855f77b53acfc92d05992f239b4b1488
Michael Lechowicz <ichal.lechowicz@gmail.com>
2021-11-18 15:41:36 +0100

    Improve button logic

--- OHMMeter.ino
+++ OHMMeter.ino
@@ -30,18 +30,7 @@ void loop()
   sensorVal = digitalRead(3);   //odczytanie stanu przycisku i przypisanie go do zmiennej sensorVal
   a2d_data=analogRead(A0); // READ data from analog A0 port (0 - 1023)
   Serial.println(sensorVal);     //przesłanie odczytanej wartości zmiennej sensorVal do portu szeregowego
- // Należy pamiętać, że po aktywacji pull-up logika przycisku jest odwrócona
-  // Jeśli przycisk jest nie naciśnięty to jego stan jest wysoki (HIGH),
-  // a stan niski (LOW) kiedy jest naciśnięty. Dioda będzie zapalona kiedy przycisk
-  // zostanie naciśnięty:
-  if (sensorVal == HIGH) {
-    lcd.noBacklight();
-    delay(1000);
-  } else {
-    lcd.backlight();
-    delay(4000);
-  }
-  
+   
   if(a2d_data)
   {
     buffer=a2d_data*Vin;
@@ -52,15 +41,25 @@ void loop()
     //Serial.println(buffer);
     R_sensor=R_reference*buffer; // calculate unknown R2 from Ohm rule
     value_percent = ((R_sensor-min_value)/range_value)*100; //calculate actual fuel in percent
-    lcd.clear();
-    lcd.setCursor(4,0);
-    lcd.print("ohm meter");
-
-    lcd.setCursor(4,1);
-    lcd.print(round(value_percent));
-    lcd.print("\% FUEL");
+    // Należy pamiętać, że po aktywacji pull-up logika przycisku jest odwrócona
+  // Jeśli przycisk jest nie naciśnięty to jego stan jest wysoki (HIGH),
+  // a stan niski (LOW) kiedy jest naciśnięty. Dioda będzie zapalona kiedy przycisk
+  // zostanie naciśnięty:
+    if (sensorVal == LOW) {
+      lcd.backlight();
+      lcd.clear();
+      lcd.setCursor(4,0);
+      lcd.print("ohm meter");
+  
+      lcd.setCursor(4,1);
+      lcd.print(round(value_percent));
+      lcd.print("\% FUEL");
+      delay(4000);
+    } else {
+      lcd.noBacklight();
+    }
+    delay(300);
+    
     Serial.println(R_sensor);
-    //delay(3000);
-    //lcd.noBacklight();
   }
 }
