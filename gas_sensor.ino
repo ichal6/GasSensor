30a54397fc803b14598f8ba5675ae992cda2642c
Michael Lechowicz <ichal.lechowicz@gmail.com>
2021-12-30 19:25:29 +0100

    Create function for calculate percent result

--- gas_sensor.ino
+++ gas_sensor.ino
@@ -14,7 +14,7 @@ float value_percent = 0;
 float max_value = 89.91;
 float min_value = 1.93;
 float range_value = max_value - min_value;
-float last_val = 30;
+float last_val = 0;
 int sensorVal = LOW;
 unsigned long prev = 0;
 float actual_expenditure = 0;
@@ -27,24 +27,26 @@ float read_from_sensor()
   
   buffer=a2d_data*Vin;
   Vout=(buffer)/1024.0; // calculate out voltage
-  //Serial.println(Vout);
   buffer=Vout/(Vin-Vout); 
-  ///Serial.print("buffer = "); 
-  //Serial.println(buffer);
   R_sensor=R_reference*buffer; // calculate unknown R2 from Ohm rule
 
   return R_sensor;
 }
 
+float calculate_percent_expenditure(float R_sensor)
+{
+  return ((R_sensor-min_value)/range_value)*100; //calculate actual fuel in percent
+}
+
 void setup() 
 {
- lcd.init();
- lcd.backlight(); //turn on backlight
- Serial.begin(9600); //start communication via UART with speed 9600 baud/sec (symbols/seconds)
- pinMode(3, INPUT_PULLUP); //configure pin 3 as an output and active built in pull up resistor
- pinMode(LED_PIN, OUTPUT); //configure built in LED on board Arduino UNO
- prev = millis();
-     
+  lcd.init();
+  lcd.backlight(); //turn on backlight
+  Serial.begin(9600); //start communication via UART with speed 9600 baud/sec (symbols/seconds)
+  pinMode(3, INPUT_PULLUP); //configure pin 3 as an output and active built in pull up resistor
+  pinMode(LED_PIN, OUTPUT); //configure built in LED on board Arduino UNO
+  prev = millis();
+  last_val = read_from_sensor();
 }
 
 void loop()
@@ -52,8 +54,7 @@ void loop()
 
   {
     R_sensor=read_from_sensor();
-    value_percent = ((R_sensor-min_value)/range_value)*100; //calculate actual fuel in percent
-    // Serial.println(R_sensor);
+    value_percent = calculate_percent_expenditure(R_sensor);
     
     if( value_percent <= 8){
       digitalWrite(LED_PIN, HIGH);
@@ -84,8 +85,9 @@ void loop()
 
   unsigned long now = millis();
 
-  if((now - prev)/1000 >= 20){
+  if((now - prev)/1000 >= 2){
     actual_expenditure = (last_val - R_sensor);
+    
     Serial.print("Aktualne zuzycie: ");
     Serial.println(actual_expenditure);
     prev = now;
