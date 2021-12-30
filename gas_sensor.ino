de0f5401bf8579adad8991d63819d175cd156b2d
Michael Lechowicz <ichal.lechowicz@gmail.com>
2021-12-30 22:21:33 +0100

    Implement liter per time

--- gas_sensor.ino
+++ gas_sensor.ino
@@ -5,14 +5,10 @@
 LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 16, 2);
 
 //GLOBAL VALUES
-int Vin=5;        //voltage at 5V pin of arduino
 float Vout=0;     //voltage at A0 pin of arduino
-float R_reference=218;    //value of known resistance
+
 
 float value_percent = 0;
-float max_value = 89.91;
-float min_value = 1.93;
-float range_value = max_value - min_value;
 float last_val = 0;
 
 int sensorVal = LOW;
@@ -24,6 +20,14 @@ float last_expenditure = 0;
 //COSTANT
 int LED_PIN = 8;
 int SIZE_TANK = 32; // size of tank in liters
+float SAMPLING = 1*60; //size in seconds
+
+float max_value = 90.3;
+float min_value = 2.4;
+float range_value = max_value - min_value;
+
+float R_reference = 219;    //value of known resistance
+int Vin=5;        //voltage at 5V pin of arduino
 
 float read_from_sensor()
 {
@@ -31,9 +35,8 @@ float read_from_sensor()
   
   float buffer=a2d_data*Vin;
   Vout=(buffer)/1024.0; // calculate out voltage
-  buffer=Vout/(Vin-Vout); 
+  buffer=Vout/(Vin-Vout);
   float R_sensor=R_reference*buffer; // calculate unknown Resistor from Ohm rule
-
   return R_sensor;
 }
 
@@ -45,11 +48,8 @@ float calculate_percent_expenditure(float R_sensor)
 void calculate_fluel_expenditure()
 {
   unsigned long now = millis();
-
-  if((now - prev)/1000 >= 2){
+  if((now - prev)/1000 >= SAMPLING){
     actual_expenditure = (last_val - value_percent);
-    Serial.print("Aktualne zuzycie: ");
-    Serial.println(actual_expenditure);
     prev = now;
 
     Serial.println(last_val);
@@ -77,18 +77,22 @@ void display_first_row()
   else 
   {
     digitalWrite(LED_PIN, LOW);
+    float value = 0;
     if(int(actual_expenditure) > 0) // display actual expenditure (the wehicle is moving)
     {
-      lcd.setCursor(3,0);
+      lcd.setCursor(0,0);
       lcd.print("FUEL USE ");
-      lcd.print((actual_expenditure*SIZE_TANK)/100, 1);
+      value = ((actual_expenditure*SIZE_TANK)/100)/(SAMPLING/3600);
+      lcd.print(value, 1);
     }
     else // display last expenditure (the wehicle is stop)
     {
       lcd.setCursor(0,0);
       lcd.print("L. F. USE ");
-      lcd.print((last_expenditure*SIZE_TANK)/100, 1);
+      value = ((last_expenditure*SIZE_TANK)/100)/(SAMPLING/3600);
+      lcd.print(value, 1);
     }
+
     lcd.print(" L");
   }
 }
