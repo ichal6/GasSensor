ce53df4526d7ec128e9f6ac4712cf6919ff175e3
Michael Lechowicz <ichal.lechowicz@gmail.com>
2021-12-30 19:54:31 +0100

    Move actual expenditure to extternal function

--- gas_sensor.ino
+++ gas_sensor.ino
@@ -19,6 +19,7 @@ int sensorVal = LOW;
 unsigned long prev = 0;
 float actual_expenditure = 0;
 int LED_PIN = 8;
+float last_expenditure = 0;
 
 
 float read_from_sensor()
@@ -36,6 +37,25 @@ float read_from_sensor()
 float calculate_percent_expenditure(float R_sensor)
 {
   return ((R_sensor-min_value)/range_value)*100; //calculate actual fuel in percent
+}
+
+void calculate_fluel_expenditure()
+{
+  unsigned long now = millis();
+
+  if((now - prev)/1000 >= 2){
+    actual_expenditure = (last_val - value_percent);
+    Serial.print("Aktualne zuzycie: ");
+    Serial.println(actual_expenditure);
+    prev = now;
+
+    Serial.println(last_val);
+    if(int(actual_expenditure) > 0)
+    {
+      last_expenditure = actual_expenditure;
+      last_val = value_percent;  
+    }
+  }
 }
 
 void setup() 
@@ -63,9 +83,18 @@ void loop()
       lcd.print("LOW FUEL!!!");
     } else{
       digitalWrite(LED_PIN, LOW);
-      lcd.setCursor(3,0);
-      lcd.print("FUEL USE ");
-      lcd.print(actual_expenditure);
+      if(int(actual_expenditure) > 0)
+      {
+        lcd.setCursor(3,0);
+        lcd.print("FUEL USE ");
+        lcd.print(actual_expenditure);
+      }
+      else
+      {
+        lcd.setCursor(0,0);
+        lcd.print("L FUEL USE ");
+        lcd.print(last_expenditure);
+      }
     }
     
     if (sensorVal == LOW) { // LOW = don't push button
@@ -83,14 +112,6 @@ void loop()
   }
   sensorVal = digitalRead(3); //read button status
   // Serial.println(sensorVal);
-
-  unsigned long now = millis();
-
-  if((now - prev)/1000 >= 2){
-    actual_expenditure = (last_val - value_percent);
-    Serial.print("Aktualne zuzycie: ");
-    Serial.println(actual_expenditure);
-    prev = now;
-    last_val = value_percent;
-  }
+  calculate_fluel_expenditure();
+ 
 }
